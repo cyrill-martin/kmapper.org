580632d6bdc80348b505abe6715b605c75e4b315
Cyrill <cyrill.martin@kmapper.com>
2024-02-22 14:37:48 +0100

    Update 'mobile' width

--- src/stores/screenSize.js
+++ src/stores/screenSize.js
@@ -7,7 +7,7 @@ export const useScreenSizeStore = defineStore("screenSize", () => {
 
   const ctrMarginV = computed(() => height.value * 0.8 * 0.01)
   const ctrMarginH = computed(() => width.value * 0.01)
-  const isMobile = computed(() => width.value < 800)
+  const isMobile = computed(() => width.value < 1000)
 
   const modalWidth = computed(() => (isMobile.value ? "95%" : "90%"))
 

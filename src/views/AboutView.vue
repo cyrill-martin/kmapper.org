58ec1711d434908191c8a50e54476b109943d1e2
Cyrill Martin <cyrill.martin@kmapper.ch>
2026-08-17 15:43:40 +0200

    Add example queries, change contacts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -14,8 +14,8 @@
     <p>This is an ongoing experiment and I welcome your feedback.</p>
     <p>
       Cyrill Martin <br />
-      <a href="https://kmapper.com" target="_blank">kmapper GmbH</a><br />
-      <a href="mailto:cyrill.martin@kmapper.com">cyrill.martin@kmapper.com</a>
+      <a href="https://kmapper.ch" target="_blank">kmapper GmbH</a><br />
+      <a href="mailto:cyrill.martin@kmapper.ch">cyrill.martin@kmapper.ch</a>
     </p>
   </n-flex>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,32 @@ import { useScreenSizeStore } from "../stores/screenSize.js"
 const screenSize = useScreenSizeStore()
 const logoSize = computed(() => (screenSize.isMobile ? "150px" : "250px"))
 const bottomMargin = computed(() => (screenSize.isMobile ? "0 0 10px 0" : "0 0 25px 0"))
+
+const exampleQueries = [
+  "urban sustainability",
+  "astrobiology",
+  "crypto rug pulls"
+]
 </script>
 
 <template>
   <n-flex vertical class="homeContainer">
     <div>
-      <img
-        :style="{ width: logoSize }"
-        src="../assets/images/kmapper_k.png"
-        alt="The kmapper logo"
-      />
+      <img :style="{ width: logoSize }" src="../assets/images/kmapper_k.png" alt="The kmapper logo" />
     </div>
-    <div :style="{ margin: bottomMargin }">
+    <div>
       Visualizing relevant <a href="https://sdgs.un.org/goals" target="_blank">SDGs</a> and research
       fields of open access works
+    </div>
+    <div :style="{ margin: bottomMargin }">
+      Example queries:
+      <ul>
+        <li v-for="(item, index) in exampleQueries" :key="index">
+          <RouterLink :to="{ path: '/map', query: { q: item } }">
+            {{ item }}
+          </RouterLink>
+        </li>
+      </ul>
     </div>
     <div>
       <TheSearch />
@@ -33,6 +45,7 @@ const bottomMargin = computed(() => (screenSize.isMobile ? "0 0 10px 0" : "0 0 2
   margin: 0 30vw;
   padding-top: 15vh;
 }
+
 @media (max-width: 1000px) {
   .homeContainer {
     margin: 0 5vw;
